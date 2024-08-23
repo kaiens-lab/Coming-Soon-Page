b97e940baa1bc69a936485d321806090c14f4c91
KN <156647897+kn070648@users.noreply.github.com>
2024-08-23 11:28:03 +0800

    Updated error-icon

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -2,40 +2,63 @@ import React, { useState } from "react";
 import "./EmailForm.css";
 import errorIcon from "../assets/images/icon-error.svg";
 
-function EmailForm() {
+const EmailForm = () => {
   const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (validateEmail(email)) {
-      setSuccess("Email submitted successfully!");
-      setError(""); // 清除錯誤消息
-    } else {
-      setError("Please provid a valid email.");
-      setSuccess(""); // 清除成功消息
-    }
-  };
+  const [message, setMessage] = useState("");
+  const [isError, setIsError] = useState(false);
 
   const validateEmail = (email) => {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!email) {
+      setMessage("Please enter email!");
+      setIsError(true);
+    } else if (!validateEmail(email)) {
+      setMessage("Please provide a valid email!");
+      setIsError(true);
+    } else {
+      setMessage("Email sent!");
+      setIsError(false);
+      setEmail("");
+    }
+  };
+
   return (
-    <form onSubmit={handleSubmit} className="input-container" noValidate>
+    <div className="email-form">
       <input
+        id="input-email"
         type="email"
-        placeholder="Email Address"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
+        className={`input-email ${
+          isError ? "border-SoftRed" : "border-DesaturatedRed"
+        }`}
       />
-      <button type="submit">{">"}</button>
-      {error && <p className="error">{error}</p>}
-      {success && <p className="success">{success}</p>}
-    </form>
+      <button id="email-btn" onClick={handleSubmit}>
+        {">"}
+      </button>
+      <span
+        id="icon-wrong"
+        className={`icon-wrong ${isError ? "" : "hidden"}`}
+        role="img"
+        aria-label="Error icon"
+      >
+        <img src={errorIcon} alt="Error" />
+      </span>
+      <p
+        id={isError ? "email-message-wrong" : "email-message-success"}
+        className={`email-message ${
+          isError ? "text-SoftRed" : "success success-message"
+        }`}
+      >
+        {message}
+      </p>
+    </div>
   );
-}
+};
 
 export default EmailForm;
